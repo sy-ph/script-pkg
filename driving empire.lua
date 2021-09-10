2210e6cae16f32ce5b17588a54941ad4fc1468f8
sy-ph <77126088+sy-ph@users.noreply.github.com>
2021-09-09 22:48:06 -0700

    Add files via upload

---
+++ driving empire.lua
@@ -0,0 +1,2 @@
+-- driving empire
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KPRAAHUE/driving-empire-autofarm2/main/README.md"))()
---
+++ vehicle simulator.lua
@@ -0,0 +1,2 @@
+-- vehicle simulator
+loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/VehicleSimulator'))()
